b9dd2d28dc5d397ea8216e9f23d6f314523364e0
Martin Angulo <martinangulo1194@gmail.com>
2022-09-06 12:43:45 -0500

    ready for deploy

--- api/src/app.js
+++ api/src/app.js
@@ -10,14 +10,15 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
+// const pass = ('http://ec2-54-172-224-92.compute-1.amazonaws.com:3000'||'http://localhost:3000')
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' })); //{ limit: '50mb' }
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://ec2-54-172-224-92.compute-1.amazonaws.com:3000');
-  //res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  // res.header('Access-Control-Allow-Origin', 'http://ec2-54-172-224-92.compute-1.amazonaws.com:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
